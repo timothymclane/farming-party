16722b2b76b7061425189a866b907c3de4f1fcdc
timothymclane <timothyamclane@gmail.com>
2017-11-05 11:13:06 -0600

    Remove t: format from zo_strformat call on logging

--- logger/Logger.lua
+++ logger/Logger.lua
@@ -29,14 +29,14 @@ function FarmingPartyLogger:LogLootItem(looterName, lootedByPlayer, itemLink, qu
     
     local lootMessage = ''
     if not lootedByPlayer then
-        lootMessage = zo_strformat("|cFFFFFF<<C:1>>|r |c228B22received|r <<t:2>>", looterName, itemText)
+        lootMessage = zo_strformat("|cFFFFFF<<C:1>>|r |c228B22received|r <<2>>", looterName, itemText)
     else
         if not FarmingParty.Settings:DisplayOwnLoot() then return end
-        lootMessage = zo_strformat("|c228B22Received|r <<t:1>>", itemText)
+        lootMessage = zo_strformat("|c228B22Received|r <<1>>", itemText)
     end
     if FarmingParty.Settings:DisplayInChat() then
         CHAT_SYSTEM:AddMessage(lootMessage)
     end
     
-    FarmingPartyWindowBuffer:AddMessage(lootMessage, 255, 255, 0, 1)
+    FarmingPartyWindowBuffer:AddMessage(lootMessage, 255, 255, 0, 1)    
 end
